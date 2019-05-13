e4d5ae2d855b71269ea7e4e0e43a19a6ab1a8cca
seikuu2 <reikuu2@yahoo.co.jp>
2019-05-14 01:29:20 +0900

    星空動くよ

---
+++ Assets/Script/StarrySky_floating.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarrySky_floating : MonoBehaviour
+{
+    Vector2 Ini_pos;
+    float MoveSize = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 動いた距離を測るために初期値を記憶
+        Ini_pos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Sinグラフでふわふわします
+        float f = 0.5f;
+        float sin = Mathf.Sin(0.5f * Mathf.PI * f * Time.time);
+
+        Vector3 pos = this.transform.position;
+        pos.x = (sin * MoveSize) + Ini_pos.x;
+        pos.y = (sin * MoveSize) + Ini_pos.y;
+
+        transform.position = pos;
+    }
+}
