2ca91dfb5136cec38dc24a637430aeb2e392d135
seikuu2 <reikuu2@yahoo.co.jp>
2019-05-24 12:23:07 +0900

    UI動くよ

--- Assets/Script/StageSelect.cs
+++ Assets/Script/StageSelect.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StageSelect : MonoBehaviour
 {
+    [SerializeField]
+    RectTransform Stage1 = null;
+    [SerializeField]
+    GameObject Stage2 = null;
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+       
+        Stage1.localPosition += new Vector3(1, 0, 0);
+    }
     public void First_1()
     {
         FadeManager.FadeOut(2);
